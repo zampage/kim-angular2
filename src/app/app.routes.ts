ded36538a9d445e0b63e8b03e4529e420884255d
Markus Chiarot <cmarkus@hotmail.ch>
2016-10-12 20:56:33 +0200

    paging setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,26 @@
 import {ModuleWithProviders} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {InventoryComponent} from "./inventory/inventory.component";
+import {RecepiesComponent} from "./recepies/recepies.component";
 
 const appRoutes : Routes = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent
+  },
+  {
+    path: 'recepies',
+    component: RecepiesComponent
   }
 ];
 
---
+++ src/app/recepies/recepies.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { RecepiesComponent } from './recepies.component';
+
+describe('Component: Recepies', () => {
+  it('should create an instance', () => {
+    let component = new RecepiesComponent();
+    expect(component).toBeTruthy();
+  });
+});
