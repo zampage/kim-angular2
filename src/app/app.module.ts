17f71c56051c59987e17b4e8e14a3278b5b83d07
Markus Chiarot <cmarkus@hotmail.ch>
2016-10-12 18:06:08 +0200

    routing implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { routing } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import {ModuleWithProviders} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+
+const appRoutes : Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  }
+];
+
+export const routing : ModuleWithProviders = RouterModule.forRoot(appRoutes);
